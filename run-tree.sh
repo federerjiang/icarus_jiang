e9eaf0c92a98b511eec4774def937382cb426aa4
federerjiang <federerjiang@gmail.com>
2017-01-30 14:06:08 +0900

    add run-tree.sh file

---
+++ run-tree.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Enable command echo
+set -v
+
+rm results.pickle
+
+
+# Run experiments
+echo "Run tree standard experiments"
+python icarus.py --results results-standard.pickle config/tree-standard.py
+echo "Simulation is finished"
+echo "Get Results: Latency and Hit Ratios"
+python printresults.py results-standard.pickle > results/results-tree-standard.txt
+echo "Results are saved to file results.txt"
+
+
+echo "Run tree edge experiments"
+python icarus.py --results results-edge.pickle config/tree-edge.py
+echo "Simulation is finished"
+echo "Get Results: Latency and Hit Ratios"
+python printresults.py results-edge.pickle > results/results-tree-edge.txt
+echo "Results are saved to file results.txt"
+
+echo "Run tree coordinated edge experiments"
+python icarus.py --results results-coor-edge.pickle config/tree-coor-edge.py
+echo "Simulation is finished"
+echo "Get Results: Latency and Hit Ratios"
+python printresults.py results-coor-edge.pickle > results/results-tree-coor-edge.txt
+echo "Results are saved to file results.txt"
