41bfcbe31a428cd6ae0d2c9d4f197c18095e00f5
federerjiang <federerjiang@gmail.com>
2017-01-31 23:11:44 +0900

    update

--- run-telstra-snm.sh
+++ run-telstra-snm.sh
@@ -8,22 +8,22 @@ set -v
 
 # Run experiments
 echo "Run telstra standard experiments"
-python icarus.py --results results-standard.pickle config/snm/telstra-standard.py
+# python icarus.py --results results-standard.pickle config/snm/telstra-standard.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-python printresults.py results-standard.pickle > results/results-telstra-snm.txt
+# python printresults.py results-standard.pickle > results/results-telstra-snm.txt
 echo "Results are saved to file results.txt"
 
 
 echo "Run telstra edge experiments"
-python icarus.py --results results-edge.pickle config/snm/telstra-edge.py
+# python icarus.py --results results-edge.pickle config/snm/telstra-edge.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-python printresults.py results-edge.pickle >> results/results-telstra-snm.txt
+# python printresults.py results-edge.pickle >> results/results-telstra-snm.txt
 echo "Results are saved to file results.txt"
 
 echo "Run telstra coordinated edge experiments"
-python icarus.py --results results-coor-edge.pickle config/snm/telstra-coor-edge.py
+python icarus.py --results results-coor-edge.pickle config/snm/telstra-coor.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
 python printresults.py results-coor-edge.pickle >> results/results-telstra-snm.txt
