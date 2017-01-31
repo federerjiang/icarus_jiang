fcb9776395d09a5fed8910b36bc798e8e5d1b6d4
federerjiang <federerjiang@gmail.com>
2017-01-31 21:40:25 +0900

    update

--- run-tree.sh
+++ run-tree.sh
@@ -11,20 +11,20 @@ echo "Run tree standard experiments"
 python icarus.py --results results-standard.pickle config/tree-standard.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-python printresults.py results-standard.pickle > results/results-tree-standard-high-5.txt
+python printresults.py results-standard.pickle > results/results-tree.txt
 echo "Results are saved to file results.txt"
 
 
 echo "Run tree edge experiments"
-# python icarus.py --results results-edge.pickle config/tree-edge.py
+python icarus.py --results results-edge.pickle config/tree-edge.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-# python printresults.py results-edge.pickle > results/results-tree-edge-5-10.txt
+python printresults.py results-edge.pickle >> results/results-tree.txt
 echo "Results are saved to file results.txt"
 
 echo "Run tree coordinated edge experiments"
-# python icarus.py --results results-coor-edge.pickle config/tree-coor-edge.py
+python icarus.py --results results-coor-edge.pickle config/tree-coor-edge.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-# python printresults.py results-coor-edge.pickle > results/results-tree-coor-branch-4.txt
+python printresults.py results-coor-edge.pickle >> results/results-tree.txt
 echo "Results are saved to file results.txt"
