48fa5913540d477d04a825e68aa5d390f71eceb7
federerjiang <federerjiang@gmail.com>
2017-02-02 10:08:01 +0900

    update

--- config/sinet-all.py
+++ config/sinet-all.py
@@ -68,7 +68,8 @@ ALPHA = [0.6]
 
 # Total size of network cache as a fraction of content population
 # Remove sizes not needed
-NETWORK_CACHE = [0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 2, 3]
+# NETWORK_CACHE = [0.1, 0.3, 0.5, 0.7, 0.9, 1.1, 1.3, 2, 3]
+NETWORK_CACHE = [0.1, 0.5, 1.0]
 
 
 STRATEGIES = [
