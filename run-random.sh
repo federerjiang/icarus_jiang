55abfab492076d9b8d365a1faa7c8a60d000e924
federerjiang <federerjiang@gmail.com>
2017-02-03 11:10:50 +0900

    update

--- run-random.sh
+++ run-random.sh
@@ -11,5 +11,5 @@ echo "Run experiments"
 python icarus.py --results results.pickle config/random.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random.txt
+python printresults.py results.pickle > results/random-sample.txt
 echo "Results are saved to file results.txt"
--- run-sinet.sh
+++ run-sinet.sh
@@ -11,5 +11,5 @@ echo "Run experiments"
 python icarus.py --results results.pickle config/sinet-all.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/sinet-all.txt
+python printresults.py results.pickle > results/sinet-all-sample.txt
 echo "Results are saved to file results.txt"
