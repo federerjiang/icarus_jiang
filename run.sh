970543267a32892352de3d81d49c13ca1cf3474d
federerjiang <federerjiang@gmail.com>
2017-02-05 11:02:16 +0900

    update

--- run.sh
+++ run.sh
@@ -3,10 +3,5 @@
 # Enable command echo
 set -v
 
-# Run experiments
-echo "Run telstra standard experiments"
-python icarus.py --results results-standard.pickle config/youtube/telstra-standard.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results-standard.pickle > results/results-telstra-youtube-standard.txt
-echo "Results are saved to file results.txt"
+# Run all experiments
+sh run-tree.sh ; sh run-tree-snm.sh; sh run-telstra.sh; sh run-telstra-snm.sh ; sh run-sinet.sh; sh run-sinet-snm.sh; sh run-geant.sh; sh run-geant-snm.sh
