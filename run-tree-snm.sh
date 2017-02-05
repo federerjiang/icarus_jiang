33b694eec9662cc0d96fa0629b3eadfa6661c47f
federerjiang <federerjiang@gmail.com>
2017-02-05 10:20:17 +0900

    update

--- run-tree-snm.sh
+++ run-tree-snm.sh
@@ -23,7 +23,7 @@ python printresults.py results-edge.pickle >> results/tree-snm.txt
 echo "Results are saved to file results.txt"
 
 echo "Run tree coordinated edge experiments"
-python icarus.py --results results-coor-edge.pickle config/snm/tree-coor-edge.py
+python icarus.py --results results-coor-edge.pickle config/snm/tree-coor.py
 echo "Simulation is finished"
 echo "Get Results: Latency and Hit Ratios"
 python printresults.py results-coor-edge.pickle >> results/tree-snm.txt
