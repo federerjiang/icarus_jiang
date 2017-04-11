7a677e433de2c2a119edff12e33536923823c033
federerjiang <federerjiang@gmail.com>
2017-04-11 10:16:26 +0900

    update

--- config/tree-edge.py
+++ config/tree-edge.py
@@ -68,7 +68,7 @@ ALPHA = [0.8]
 
 # Total size of network cache as a fraction of content population
 # Remove sizes not needed
-NETWORK_CACHE = [0.02, 0.05, 0.1, 0.2, 0.3, 0.5]
+NETWORK_CACHE = [0.01]
 # NETWORK_CACHE = [0.3]
 
 
