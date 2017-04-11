ea590d6fb4b153351f233bdbbd0b1a86576029ee
federerjiang <federerjiang@gmail.com>
2017-04-11 16:03:09 +0900

    update

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 set -v
 
 # Run all experiments
-python icarus.py --results results.pickle config/tree-edge.py; python printresults.py results.pickle > results/tree-edge-irm.txt
+python icarus.py --results results.pickle config/tree-edge.py; python printresults.py results.pickle >> results/tree-edge-irm.txt
