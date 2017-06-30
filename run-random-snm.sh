35a3561e746a3f9b644f844d349091d26112a936
federerjiang <federerjiang@gmail.com>
2017-07-01 06:04:00 +0900

    update

--- run-random-snm.sh
+++ run-random-snm.sh
@@ -7,12 +7,12 @@ rm results.pickle
 
 
 # Run experiments
-echo "Run experiments"
-python icarus.py --results results.pickle config/snm/random.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random-snm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/snm/random.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random-snm.txt
+# echo "Results are saved to file results.txt"
 
 echo "Run experiments"
 python icarus.py --results results.pickle config/snm/random1.py
@@ -21,16 +21,16 @@ echo "Get Results: Latency and Hit Ratios"
 python printresults.py results.pickle > results/random1-snm.txt
 echo "Results are saved to file results.txt"
 
-echo "Run experiments"
-python icarus.py --results results.pickle config/snm/random2.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random2-snm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/snm/random2.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random2-snm.txt
+# echo "Results are saved to file results.txt"
 
-echo "Run experiments"
-python icarus.py --results results.pickle config/snm/random3.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random3-snm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/snm/random3.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random3-snm.txt
+# echo "Results are saved to file results.txt"
--- run-random.sh
+++ run-random.sh
@@ -7,12 +7,12 @@ rm results.pickle
 
 
 # Run experiments
-echo "Run experiments"
-python icarus.py --results results.pickle config/random.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random-irm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/random.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random-irm.txt
+# echo "Results are saved to file results.txt"
 
 
 echo "Run experiments"
@@ -22,16 +22,16 @@ echo "Get Results: Latency and Hit Ratios"
 python printresults.py results.pickle > results/random1-irm.txt
 echo "Results are saved to file results.txt"
 
-echo "Run experiments"
-python icarus.py --results results.pickle config/random2.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random2-irm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/random2.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random2-irm.txt
+# echo "Results are saved to file results.txt"
 
-echo "Run experiments"
-python icarus.py --results results.pickle config/random3.py
-echo "Simulation is finished"
-echo "Get Results: Latency and Hit Ratios"
-python printresults.py results.pickle > results/random3-irm.txt
-echo "Results are saved to file results.txt"
+# echo "Run experiments"
+# python icarus.py --results results.pickle config/random3.py
+# echo "Simulation is finished"
+# echo "Get Results: Latency and Hit Ratios"
+# python printresults.py results.pickle > results/random3-irm.txt
+# echo "Results are saved to file results.txt"
